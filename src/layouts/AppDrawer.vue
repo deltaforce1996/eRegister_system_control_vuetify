12e50b96f1a07c3e32a348798641744fe0ab9889
Alif <deltaforce19436@gmail.com>
2023-12-01 01:01:50 +0700

    icon navbar

--- src/layouts/AppDrawer.vue
+++ src/layouts/AppDrawer.vue
@@ -27,7 +27,12 @@ const setActiveItem = (clickedItem) => {
 
 const items = [
   { id: 1, icon: "mdi mdi-information", isActive: false, path: "/" },
-  { id: 2, icon: "mdi-account-box", isActive: false, path: "/UserManagement" },
-  { id: 3, icon: "mdi-cog", isActive: false, path: "/ListRolesPage" },
+  { id: 2, icon: "mdi mdi-account-cog", isActive: false, path: "/UserManagement" },
+  { id: 3, icon: "mdi mdi-account-lock", isActive: false, path: "/ListRolesPage" },
+  { id: 4, icon: "mdi mdi-clipboard-list", isActive: false, path: "/" },
+  { id: 5, icon: "mdi mdi-text-search-variant", isActive: false, path: "/" },
+  { id: 6, icon: "mdi mdi-chart-timeline-variant", isActive: false, path: "/" },
+  { id: 7, icon: "mdi mdi-account-group", isActive: false, path: "/" },
+  { id: 8, icon: "mdi mdi-help-circle", isActive: false, path: "/" },
 ];
 </script>
