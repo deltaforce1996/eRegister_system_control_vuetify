fb905ec1abbe945831f6c171283a0a4d8c6a4c8d
farook <farookstyle.bnk@gmail.com>
2024-01-09 00:08:35 +0700

    traning

--- src/views/SDTeamMangement/TraningCreated.vue
+++ src/views/SDTeamMangement/TraningCreated.vue
@@ -1,16 +1,28 @@
 <template>
   <v-container>
-    <h2>Mangement > RSP Policy</h2>
+    <h2>Mangement > Traning</h2>
     <div class="mt-5">
       <v-card class="elevation-1">
         <v-card-text>
           <div>
-            <strong>ชื่อ policy</strong>
-            <v-text-field variant="outlined" placeholder="ชื่อ policy" density="compact"></v-text-field>
+            <strong>ชื่อเรื่อง</strong>
+            <v-text-field variant="outlined" placeholder="ชื่อกรอกชื่อเรื่อง" density="compact"></v-text-field>
           </div>
-          <Choosefile @input-file="handleOnChangeFile" :is-file="file" title="อัพโหลด policy" list-title="uploaded Files"
-            descritption="อัพโหลดเฉพาะไฟล์ PDF จำนวนไฟล์ เท่านั้น">
-          </Choosefile>
+          <div>
+            <strong>Link SCORM</strong>
+            <v-text-field variant="outlined" placeholder="กรอก Link SCORM" density="compact"></v-text-field>
+          </div>
+          <div>
+            <strong>Role</strong>
+            <v-select
+                variant="outlined"
+                density="compact"
+                placeholder="Role"
+                item-title="name"
+                item-value="id"
+              ></v-select>
+          </div>
+
         </v-card-text>
        <v-radio-group>
           <template v-slot:label>
@@ -31,7 +43,12 @@
                 <strong class="text-black">Active วันที่ </strong>
               </v-col>
               <v-col cols="9" align-self="center">
-                <MenuDatePickerControl width="500" :value="date" @value="date = $event" />
+                <MenuDatePickerControl
+                width="500"
+                :value="date"
+                @value="date = $event"
+                placeholder="Active Date"
+                 />
               </v-col>
               </v-row>
               </div>
@@ -41,7 +58,7 @@
       </v-card>
       <div class="text-center mt-5">
         <v-btn rounded class="ma-2"
-        to="/SDTeamMangement/RspPolicy"
+        to="/SDTeamMangement/Traning"
         color="black" style="width: 100px;">
           <strong>ยกเลิก</strong>
         </v-btn>
@@ -55,8 +72,10 @@
 <script setup>
 // eslint-disable-next-line no-unused-vars
 import { ref, computed } from "vue";
-import Choosefile from '@/components/forms/Choosefile'
+//import Choosefile from '@/components/forms/Choosefile'
 import MenuDatePickerControl from '@/components/controls/MenuDatePickerControl.vue'
+import { useConfirmationDialog } from '@/components/dialogs/ConfirmationDialogService'
+const { showDialog } = useConfirmationDialog();
 // const emit = defineEmits(["visible"]);
 // const props = defineProps({
 //   visible: {
@@ -65,20 +84,24 @@ import MenuDatePickerControl from '@/components/controls/MenuDatePickerControl.v
 //   },
 // });
 const date = ref(null);
-const file = ref(null);
+// const file = ref(null);
 // const dialog = computed({
 //   get() { return props.visible },
 //   set(value) { emit("visible", value) },
 // });
 
-const handleOnChangeFile = async (val) => {
-  file.value = val;
-}
-const handleAccept = () =>{
-
-}
-// const handleUpload = () => {
-//   console.log("upload")
+// const handleOnChangeFile = async (val) => {
+//   file.value = val;
 // }
+const handleAccept = async () =>{
+
+  const confirmed = await showDialog('ยืนยัน Active ตอนนี้','กรุณาตรวจสอบคลิกปุ่ม "ตกลง" เพื่อดำเนินการ');
+      if (confirmed) {
+        console.log('Action confirmed!');
+      } else {
+        console.log('Action cancelled.');
+      }
+}
+
 </script>
 
