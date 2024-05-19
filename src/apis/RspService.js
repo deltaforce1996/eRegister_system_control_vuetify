74b0ea52ba5bb6c27e99476085ba3fa1a4a89175
Alif <deltaforce19436@gmail.com>
2024-05-19 22:39:05 +0700

    service rsp

--- src/apis/RspService.js
+++ src/apis/RspService.js
@@ -1,5 +1,17 @@
 import axiosBase from "./AxiosConfig";
-const getVendorRspStatus = async (offset = 0, limit = 1000, search_field,search_value,company_id,bu_id,company_category_id,activity_id,activity_status,completed_from,completed_to) => {
+const getVendorRspStatus = async (
+  offset = 0,
+  limit = 1000,
+  search_field,
+  search_value,
+  company_id,
+  bu_id,
+  company_category_id,
+  activity_id,
+  activity_status,
+  completed_from,
+  completed_to
+) => {
   return await axiosBase({
     method: "get",
     url: `/rsp/get-vendor-rsp-status?offset=${offset}&limit=${limit}&search_field=${search_field}&search_value=${search_value}&company_id=${company_id}&bu_id=${bu_id}&company_category_id=${company_category_id}&activity_id=${activity_id}&activity_status=${activity_status}&completed_from=${completed_from}&completed_to=${completed_to}`,
@@ -21,31 +33,36 @@ const getRspReportData = async () => {
     data: {},
   });
 };
-const sendFollowUpVendors = async (bp_numbers,is_sent,additional_msg) => {
+const sendFollowUpVendors = async (bp_numbers, is_sent, additional_msg) => {
   return await axiosBase({
     method: "post",
     url: `/rsp/follow-up-vendors`,
     data: {
-      business_partners : Array.from(bp_numbers,email=>{
-         return {
-          bp_number : email
-         }
+      business_partners: Array.from(bp_numbers, (email) => {
+        return {
+          bp_number: email,
+        };
       }),
       is_sent_to_contact_owner: is_sent,
-      additional_message : additional_msg
+      additional_message: additional_msg,
     },
   });
 };
-const sendFollowUpVendor = async (bp_number,is_sent,email,additional_msg) => {
+const sendFollowUpVendor = async (
+  bp_number,
+  is_sent,
+  email,
+  additional_msg
+) => {
   return await axiosBase({
     method: "post",
     url: `/rsp/follow-up-vendor`,
     data: {
-      "bp_number": bp_number,
-      "is_sent_to_contact_owner": is_sent,
-      "email": email,
-      "additional_message": additional_msg
-     },
+      bp_number: bp_number,
+      is_sent_to_contact_owner: is_sent,
+      email: email,
+      additional_message: additional_msg,
+    },
   });
 };
 const getRspPolicyResults = async (_bp_number) => {
@@ -53,7 +70,7 @@ const getRspPolicyResults = async (_bp_number) => {
     method: "get",
     url: `/rsp/get-rsp-policy-results`,
     params: {
-      bp_number : _bp_number
+      bp_number: _bp_number,
     },
   });
 };
@@ -62,27 +79,27 @@ const getRspSurveyResults = async (_bp_number) => {
     method: "get",
     url: `/rsp/get-rsp-survey-results`,
     params: {
-      bp_number : _bp_number
+      bp_number: _bp_number,
     },
   });
 };
+
 const getRspSurveyResultDetail = async (_bp_number) => {
   return await axiosBase({
     method: "get",
     url: `/rsp/get-rsp-survey-result-detail`,
     params: {
-      bp_number : _bp_number
+      bp_number: _bp_number,
     },
   });
-
 };
 const getRspSurveyResultDocument = async (_bp_number, _rsp_suvery_id) => {
   return await axiosBase({
     method: "get",
     url: `/rsp/get-rsp-survey-result-documents`,
     params: {
-      bp_number :_bp_number,
-      rsp_suvery_id : _rsp_suvery_id
+      bp_number: _bp_number,
+      rsp_suvery_id: _rsp_suvery_id,
     },
   });
 };
@@ -91,9 +108,11 @@ const exportRspActivityReport = async (bp_number = []) => {
     method: "post",
     url: `/rsp/export-rsp-activity-report`,
     data: {
-      business_partner : Array.from(bp_number,i =>{ return {
-        bp_number: i
-      }})
+      business_partner: Array.from(bp_number, (i) => {
+        return {
+          bp_number: i,
+        };
+      }),
     },
   });
 };
@@ -118,43 +137,43 @@ const getRspPolicyState = async (_state) => {
     method: "get",
     url: `/rsp/get-rsp-policies`,
     params: {
-      state : _state
+      state: _state,
     },
   });
 };
-const getRspPolicyPagination = async (_state,_offset,_limit) => {
+const getRspPolicyPagination = async (_state, _offset, _limit) => {
   return await axiosBase({
     method: "get",
     url: `/rsp/get-rsp-policies`,
     params: {
-      state:_state,
-      offset:_offset,
-      limit : _limit
+      state: _state,
+      offset: _offset,
+      limit: _limit,
     },
   });
 };
-const createRspPolicy = async (_name,_file,_start_at) => {
+const createRspPolicy = async (_name, _file, _start_at) => {
   return await axiosBase({
     method: "post",
     url: `/rsp/create-rsp-policy`,
     data: {
-      name : _name,
-      file : _file,
-      start_at : _start_at,
-      updated_user_id : 1
+      name: _name,
+      file: _file,
+      start_at: _start_at,
+      updated_user_id: 1,
     },
   });
 };
-const updateRspPolicy = async ( _rsp_policy_id,_name,_file,_start_at) => {
+const updateRspPolicy = async (_rsp_policy_id, _name, _file, _start_at) => {
   return await axiosBase({
     method: "post",
     url: `/rsp/update-rsp-policy`,
     data: {
-      rsp_policy_id : _rsp_policy_id,
-      name : _name,
-      file : _file,
-      start_at : _start_at,
-      updated_user_id : 1
+      rsp_policy_id: _rsp_policy_id,
+      name: _name,
+      file: _file,
+      start_at: _start_at,
+      updated_user_id: 1,
     },
   });
 };
@@ -188,45 +207,58 @@ const PermanentlyDeleteRspRolicy = async (_rsp_policy_id) => {
 };
 
 // traning
-const getRspTraining = async (_state,_offset,_limit,_roleId,_dateFrom,_dateTo) => {
+const getRspTraining = async (
+  _state,
+  _offset,
+  _limit,
+  _roleId,
+  _dateFrom,
+  _dateTo
+) => {
   return await axiosBase({
     method: "get",
     url: `/rsp/get-rsp-training`,
     data: {
-      state:_state,
-      offset : _offset,
-      limit : _limit,
-      role_id : _roleId,
-      created_from : _dateFrom,
-      created_to:_dateTo
+      state: _state,
+      offset: _offset,
+      limit: _limit,
+      role_id: _roleId,
+      created_from: _dateFrom,
+      created_to: _dateTo,
     },
   });
 };
-const createRspTraining = async (name,file,role_id,active_at) => {
+const createRspTraining = async (name, file, role_id, active_at) => {
   return await axiosBase({
     method: "post",
     url: `/rsp/create-rsp-training`,
     data: {
-      "name": name,
-      "data": file,
-      "role_id": role_id,
-      "published_at": active_at,
-      "created_user_id": 123
+      name: name,
+      data: file,
+      role_id: role_id,
+      published_at: active_at,
+      created_user_id: 123,
     },
   });
 };
 
-const updateRspTraining = async (training_id,name,file,role_id,active_at) => {
+const updateRspTraining = async (
+  training_id,
+  name,
+  file,
+  role_id,
+  active_at
+) => {
   return await axiosBase({
     method: "post",
     url: `/rsp/update-rsp-training`,
     data: {
-      "rsp_training_id":training_id,
-      "name": name,
-      "data": file,
-      "role_id": role_id,
-      "published_at": active_at,
-      "updated_user_id": 123
+      rsp_training_id: training_id,
+      name: name,
+      data: file,
+      role_id: role_id,
+      published_at: active_at,
+      updated_user_id: 123,
     },
   });
 };
@@ -239,133 +271,221 @@ const exportRspTrainingResult = async (rsp_training_id) => {
   });
 };
 
-
 const activateRspTraining = async (_rsp_traning_id) => {
   return await axiosBase({
     method: "post",
     url: `/rsp/activate-rsp-training`,
     data: {
-      "rsp_training_id": _rsp_traning_id,
-      "updated_user_id": "1"
-    }
+      rsp_training_id: _rsp_traning_id,
+      updated_user_id: "1",
+    },
   });
 };
-const deactivateRspTraining= async (_rsp_traning_id) => {
+const deactivateRspTraining = async (_rsp_traning_id) => {
   return await axiosBase({
     method: "post",
     url: `/rsp/deactivate-rsp-training`,
     data: {
-      "rsp_training_id": _rsp_traning_id,
-      "updated_user_id": "1"
-    }
+      rsp_training_id: _rsp_traning_id,
+      updated_user_id: "1",
+    },
   });
 };
-const undeleteRspTraining= async (_rsp_traning_id) => {
+const undeleteRspTraining = async (_rsp_traning_id) => {
   return await axiosBase({
     method: "post",
     url: `/rsp/undelete-rsp-training`,
     data: {
-      "rsp_training_id": _rsp_traning_id,
-      "updated_user_id": "1"
-    }
+      rsp_training_id: _rsp_traning_id,
+      updated_user_id: "1",
+    },
   });
 };
-const deleteRspTraining= async (_rsp_traning_id) => {
+const deleteRspTraining = async (_rsp_traning_id) => {
   return await axiosBase({
     method: "post",
     url: `/rsp/delete-rsp-training`,
     data: {
-      "rsp_training_id": _rsp_traning_id,
-      "updated_user_id": "1"
-    }
+      rsp_training_id: _rsp_traning_id,
+      updated_user_id: "1",
+    },
   });
 };
 
-const permanentlyDeleteRspTraining= async (_rsp_traning_id) => {
+const permanentlyDeleteRspTraining = async (_rsp_traning_id) => {
   return await axiosBase({
     method: "post",
     url: `/rsp/permanently-delete-rsp-training`,
     data: {
-      "rsp_training_id": _rsp_traning_id,
-      "updated_user_id": "1"
-    }
+      rsp_training_id: _rsp_traning_id,
+      updated_user_id: "1",
+    },
   });
 };
 
 // survey
+const completeRspSurvey = async (bp_number, rsp_survey_id) => {
+  return await axiosBase({
+    method: "post",
+    url: `/rsp/complete-rsp-survey`,
+    data: {
+      bp_number,
+      rsp_survey_id,
+      created_user_id: 1,
+    },
+  });
+};
 
-const getRspSurveysActive =async ()=>{
+const createRspSurveyAnswer = async (rsp_survey_result_id, answers) => {
+  // answers = [{question_id, answer}]
+  return await axiosBase({
+    method: "post",
+    url: `/rsp/create-rsp-survey-answer`,
+    data: { rsp_survey_result_id, answers, created_user_id: 1 },
+  });
+};
+
+const updateRspSurveyAnswer = async (rsp_survey_result_id, answers) => {
+  // answers = [{"question_id": 1, "answer": "ข้อ a"}]
+  return await axiosBase({
+    method: "post",
+    url: `/rsp/update-rsp-survey-answer`,
+    data: {
+      rsp_survey_result_id,
+      answers,
+      updated_user_id: 1,
+    },
+  });
+};
+
+const updateRspSurveyResult = async (
+  bp_number,
+  rsp_survey_id,
+  rsp_activity_status_id,
+  inprogress_section_id
+) => {
+  return await axiosBase({
+    method: "post",
+    url: `/rsp/update-rsp-survey-result`,
+    data: {
+      bp_number,
+      rsp_survey_id,
+      rsp_activity_status_id,
+      inprogress_section_id,
+      updated_user_id: 1,
+    },
+  });
+};
+
+const createRspSurveyResult = async (bp_number, rsp_survey_id) => {
+  return await axiosBase({
+    method: "post",
+    url: `/rsp/create-rsp-survey-result`,
+    data: { bp_number, rsp_survey_id, created_user_id: 1 },
+  });
+};
+
+const getRspSurveyAnswers = async (bp_number, rsp_survey_id, section_id) => {
+  return await axiosBase({
+    method: "get",
+    url: `/rsp/get-rsp-survey-answers?bp_number=${bp_number}&rsp_survey_id=${rsp_survey_id}&section_id=${section_id}`,
+    data: {},
+  });
+};
+
+const createRspActivityLog = async (bp_number, rsp_activity_id, is_skipped) => {
+  return await axiosBase({
+    method: "post",
+    url: `/rsp/create-rsp-activity-log`,
+    data: {
+      bp_number,
+      rsp_activity_id,
+      is_skipped,
+      created_user_id: 1,
+    },
+  });
+};
+
+const getRspSurveyResultByRspSurvayId = async (bp_number, rsp_survey_id) => {
+  return await axiosBase({
+    method: "post",
+    url: `/rsp/get-rsp-survey-results`,
+    data: {
+      bp_number,
+      rsp_survey_id,
+    },
+  });
+};
+
+const getRspSurveysActive = async () => {
   return await axiosBase({
     method: "get",
     url: `/rsp/get-rsp-surveys`,
     params: {
-      state: 'active',
-    }
+      state: "active",
+    },
   });
-}
-const getRspSurveys = async (_state,_offset,_limit)=>{
+};
+const getRspSurveys = async (_state, _offset, _limit) => {
   return await axiosBase({
     method: "get",
     url: `/rsp/get-rsp-surveys`,
     params: {
       state: _state,
-      offset : _offset,
-      limit : _limit
-    }
+      offset: _offset,
+      limit: _limit,
+    },
   });
-}
-const getRspSurveyQuestionaire = async(survey_id)=>{
+};
+
+const getRspSurveyQuestionaire = async (survey_id) => {
   return await axiosBase({
     method: "get",
-    url: `/rsp/get-rsp-survey-questionaire/${survey_id}`,
-    data: {}
+    url: `/rsp/get-rsp-survey-questionnaire/${survey_id}`,
+    data: {},
   });
-}
+};
 
-
-const deleteRspSurvey = async(survey_id)=>{
+const deleteRspSurvey = async (survey_id) => {
   return await axiosBase({
     method: "get",
     url: `/rsp/delete-rsp-survey`,
     data: {
-      "rsp_survey_id": survey_id,
-       "updated_user_id": "1"
-    }
+      rsp_survey_id: survey_id,
+      updated_user_id: "1",
+    },
   });
-}
+};
 
-const undeleteRspSurvey = async(survey_id)=>{
+const undeleteRspSurvey = async (survey_id) => {
   return await axiosBase({
     method: "get",
     url: `/rsp/undelete-rsp-survey`,
     data: {
-      "rsp_survey_id": survey_id,
-       "updated_user_id": "1"
-    }
+      rsp_survey_id: survey_id,
+      updated_user_id: "1",
+    },
   });
-}
+};
 
-const permanentlyDeleteRspSurvey = async(survey_id)=>{
+const permanentlyDeleteRspSurvey = async (survey_id) => {
   return await axiosBase({
     method: "get",
     url: `/rsp/permanently-delete-rsp-survey`,
     data: {
-      "rsp_survey_id": survey_id,
-       "updated_user_id": "1"
-    }
+      rsp_survey_id: survey_id,
+      updated_user_id: "1",
+    },
   });
-}
+};
 
-
-
-
-const exportRspSurveyResult= async (survey_id)=>{
+const exportRspSurveyResult = async (survey_id) => {
   return await axiosBase({
     method: "get",
     url: `/rsp/export-rsp-survey-result/${survey_id}`,
-    data: {}
+    data: {},
   });
-}
+};
 
 export default {
   getVendorRspStatus,
@@ -409,5 +529,13 @@ export default {
   deleteRspSurvey,
   undeleteRspSurvey,
   permanentlyDeleteRspSurvey,
-  exportRspSurveyResult
+  exportRspSurveyResult,
+  getRspSurveyResultByRspSurvayId,
+  createRspActivityLog,
+  createRspSurveyAnswer,
+  completeRspSurvey,
+  updateRspSurveyResult,
+  createRspSurveyResult,
+  updateRspSurveyAnswer,
+  getRspSurveyAnswers
 };
